9d5629bfbba7d143088f202f3dc732b3e3ffc1b7
SAMV37 <samvelpetros@gmail.com>
2017-09-07 18:36:32 +0400

    Icon added Bugs fixed

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
-
-    var a = parseInt(prompt("Insert number A"));
-    var b = parseInt(prompt("Insert number B"));
+    var a = parseInt(prompt("Insert number 1"));
+    var b = parseInt(prompt("Insert number 2"));
 
     if (isNaN(a) || isNaN(b)) {
         alert("A...A...A    Insert a number !!!");
     }
     else {
-        alert("Look the console (Ctrl+Shift+I)");
-        console.log("A + B = " + (a + b));
-        console.log("A - B = " + (a - b));
-        console.log("A * B = " + (a * b));
-        console.log("A / B = " + (a / b));
+        console.log("Number 1 + Number 2 = " + (a + b));
+        console.log("Number 1 - Number 2 = " + (a - b));
+        console.log("Number 1 * Number 2 = " + (a * b));
+        if(b === 0){
+            console.log("Because Number 2 is 0, Number 1 can't be divided into it");
+        }else {
+            console.log("Number 1 / Number 2 = " + (a / b));
+        }
+        myFunc();
     }
-
 });
+
+function myFunc(){
+    alert("Look the console (Ctrl+Shift+I)");
+}
